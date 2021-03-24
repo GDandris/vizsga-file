d6c1e9496e2a504b8bec5ed1ec4ccdd8b529309b
GDandris <andris.gede@gmail.com>
2021-03-24 15:45:34 +0100

    tasks done

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,33 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
-    }
+    public List<String> getCompetitorsNamesFromFile(String fileName) {
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+            Map<String, Integer> competitors = new HashMap<>();
+            while (myReader.hasNextLine()) {
+                String[] line = myReader.nextLine().split(",");
+                String name = line[0];
+                int points = (Integer.parseInt(line[1]) + Integer.parseInt(line[2])
+                        + Integer.parseInt(line[3]) + Integer.parseInt(line[4]) + Integer.parseInt(line[5]));
+                competitors.put(name, points);
+            }
+            myReader.close();
+            return competitors.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
 
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            return null;
+        }
+    }
 }
